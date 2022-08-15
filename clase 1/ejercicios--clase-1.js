06e4a28c2a6f88e4110d5602f753425e914baf0c
Franco1312 <francokail2@gmail.com>
2022-08-15 02:43:54 -0300

    functions refactor

--- clase 1/ejercicios--clase-1.js
+++ clase 1/ejercicios--clase-1.js
@@ -1,20 +1,25 @@
+import { números } from "../utils/constants"
 
 /* TAREA
-Tenemos que llenar esta funcion de tal manera, que nos devuelva solamente los números que son mayores a 5
+Tenemos que llenar esta funcion de tal manera, que recorra el array que llega por parámetro y
+nos devuelva un nuevo array SOLO con los números mayores a 5. 
 NOTA : NO OLVIDEMOS QUE PARA DEVOLVER, TENEMOS QUE USAR UN RETURN
 NOTA 2 : NO BORRAR NADA DE LO QUE YA EXISTE
 */
 
-const números = [1, 2, 45, 67, 78, 3, 4, 89, 5, 67, 12]
-
-function devolverNumerosMayoresACinco(números) {
+export function devolverNumerosMayoresACinco(números) {
 
     //ACÁ VA TU CÓDIGO
 }
-
 devolverNumerosMayoresACinco(números)
 
 
+/* EN ESTA FUNCION VAMOS A RECIBIR POR PARAMETROS UN ARRAY DE NUMEROS Y TENEMOS QUE DEVOLVER LA SUMA DE TODOS LOS NÚMEROS DEL ARRAY
+NOTA : NO TENEMOS QUE DEVOLVER UN NUEVO ARRAY, SI NO, UN NUEVO NUMERO QUE SEA LA SUMA DE LOS QUE VIENEN POR PARAMETRO.
+*/
 
+export function sumar(números) {
 
-module.exports = devolverNumerosMayoresACinco
+    //ACÁ VA TU CÓDIGO
+}
+sumar(números)
--- clase 1/ejercicios-clase-1.spec.js
+++ clase 1/ejercicios-clase-1.spec.js
@@ -1,13 +1,23 @@
-const devolverNumerosMayoresACinco = require('./ejercicios--clase-1');
+import { devolverNumerosMayoresACinco } from './ejercicios--clase-1'
+import { sumar } from './ejercicios--clase-1'
 
 describe('[CLASE 1]', () => {
 
+    const mockNúmeros = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
+
     it('return numbers greater than 5 ', () => {
-        const mockNúmeros = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
 
         const response = devolverNumerosMayoresACinco(mockNúmeros)
         expect(response.length).toBe(5)
         expect(response).toEqual([6, 7, 8, 9, 10])
+    })
+
+    it('sum numbers ', () => {
+        
+        const response = sumar(mockNúmeros)
+
+        expect(response).toEqual(55)
+        expect(response).toBeDefined()
     })
 
 })
